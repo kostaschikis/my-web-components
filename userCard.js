8fbdbb41a022a3b61a144af6ee588f0daf418ff4
Kostas Chikimtzis <kostaschikis@gmail.com>
2020-03-22 21:14:25 +0200

    Attached ShadowDOM

--- userCard.js
+++ userCard.js
@@ -1,7 +1,26 @@
+const template = document.createElement('template');
+template.innerHTML = `
+    <style>
+        h3 {
+            color: colar;
+        }
+    </style>
+    <div class="user-card">
+    <img />
+    <div>
+        <h3></h3>
+    </div>    
+    </div>
+`;
+
 class UserCard extends HTMLElement {
     constructor() {
         super();
-        this.innerHTML = `John Doe`;
+
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
+        this.shadowRoot.querySelector('img').src = this.getAttribute('avatar');
     }
 }
 
