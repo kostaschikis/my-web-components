e40d374955e790ef8ea895c48c05bbdaa0e646d7
Kostas Chikimtzis <kostaschikis@gmail.com>
2020-03-22 21:25:19 +0200

    Added slots

--- userCard.js
+++ userCard.js
@@ -1,14 +1,39 @@
 const template = document.createElement('template');
 template.innerHTML = `
     <style>
-        h3 {
-            color: colar;
+        .user-card {
+            font-family: Arial, sans-serif;
+            background: #f4f4f4;
+            width: 500px;
+            display: grid;
+            grid-template-columns: 1fr 2fr;
+            grid-gap: 10px;
+            margin-bottom: 15px;
+            border-bottom: darkorchid 5px solid;
+        }
+        
+        .user-card img {
+            width: 100%;
+        }
+        
+        .user-card button {
+            cursor: pointer;
+            background: darkorchid;
+            color: #fff;
+            border: 0;
+            border-radius: 5px;
+            padding: 5px 10px;
         }
     </style>
     <div class="user-card">
     <img />
     <div>
         <h3></h3>
+        <div class="info">
+            <p><slot name="email"/></p>
+            <p><slot name="phone"/></p>
+        </div> 
+        <button id="toggle-info">Hide Info</button>
     </div>    
     </div>
 `;
@@ -17,8 +42,11 @@ class UserCard extends HTMLElement {
     constructor() {
         super();
 
+        // Attaach ShadowDOM
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        // Get Attributes
         this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
         this.shadowRoot.querySelector('img').src = this.getAttribute('avatar');
     }
