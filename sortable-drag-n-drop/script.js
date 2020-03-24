6a2918f5e5e9219d7b225ba68185538c13269c38
Kostas Chikimtzis <kostaschikis@gmail.com>
2020-03-24 16:42:28 +0200

    Code Added | No Component Yet

---
+++ sortable-drag-n-drop/script.js
@@ -0,0 +1,54 @@
+const draggables = document.querySelectorAll('.draggable')
+const containers = document.querySelectorAll('.container')
+
+// Draggable Opacity 
+draggables.forEach(draggable => {
+    // When Drag Start -> Element Opacity 0.5
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging')
+    })
+    
+    // When Drag End -> Element Opacity 1
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging')
+    })
+})
+
+containers.forEach(container => {
+    container.addEventListener('dragover', e => {
+        // By defualt dropping an element is not allowed
+        e.preventDefault()
+        const afterElement = getDragAfterElement(container, e.clientY)
+
+        if (afterElement == null) {
+            container.appendChild(draggable)
+        } else {
+            container.insertBefore(draggable, afterElement)
+        }
+
+        // Get the element we are currently dragging
+        const draggable = document.querySelector('.dragging')
+        container.appendChild(draggable)
+    })
+})
+
+/**
+ * Figure out which element we are placing this in between
+ * @param container 
+ * @param mouse.y.position 
+ */
+function getDragAfterElement(container, y) {
+    // Get every element we are not currently dragging inside the container  
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        } else {
+            return closest
+        }
+
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+}
